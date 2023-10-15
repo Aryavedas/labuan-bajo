b40a2de131ff6d50cb54c2283236dca02af447e2
Arya Veda Setyanindito <vedakhansa@gmail.com>
2023-10-15 17:07:56 +0700

    [feat] compleate the landing page desktop

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const navSm = document.getElementById("nav-sm")
 
 // nav lg node
 const navLg = document.getElementById("nav-lg")
+const navLgLogo = document.getElementById("nav-logo-lg")
+const navLgList = document.getElementById("nav-list-lg")
 
 // Mengambil elemen dengan ID nav-list-sm
 const navList = document.getElementById("nav-list-sm");
@@ -26,6 +28,7 @@ navHamburger.addEventListener("click", function () {
     if (navList.classList.contains("d-none")) {
         navList.classList.remove("d-none");
         navSm.classList.remove("bg-transparent");
+        navSm.classList.add("custom-shadow");
         htmlElement.style.overflowY = 'hidden';
     } else {
         navList.classList.add("d-none");
@@ -34,13 +37,46 @@ navHamburger.addEventListener("click", function () {
     }
 });
 
-// Menambahkan event listener untuk mengubah latar belakang navbar saat scrolling
+// mengubah latar belakang navbar saat scrolling
 window.addEventListener('scroll', () => {
     if (window.scrollY > 1) {
         navSm.classList.remove("bg-transparent");
+        navSm.classList.add("custom-shadow");
         navLg.classList.remove("bg-transparent");
+        navLg.classList.add("shadow-sm");
+        navLgLogo.classList.remove("text-white");
+        navLgList.classList.remove("text-white");
     } else {
         navSm.classList.add("bg-transparent");
+        navSm.classList.remove("custom-shadow");
         navLg.classList.add("bg-transparent");
+        navLg.classList.remove("shadow-sm");
+        navLgLogo.classList.add("text-white");
+        navLgList.classList.add("text-white");
     }
+});
+
+// Margin if click Navbar
+const links = document.querySelectorAll('a[href^="#"]');
+const offset = 95;
+
+links.forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substr(1); // Mengambil ID target dari tautan
+        const targetElement = document.getElementById(targetId);
+        if (targetElement) {
+            const targetOffset = targetElement.offsetTop - offset;
+
+            window.scrollTo({
+                top: targetOffset,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
+
+const coba = document.querySelector(".modal-dialog span");
+coba.addEventListener("click", function() {
+    console.log('jancok');
 });
